712046a0b491172c29596ef6ea6f8fe8ca1d03e7
Gezellligheid_ <elias6503@gmail.com>
2026-02-25 15:20:31 +0100

    css

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,6 @@ export default defineConfig({
       output: {
         chunkFileNames: "assets/[hash].js",
         entryFileNames: "assets/[hash].js",
-        assetFileNames: "assets/[hash][extname]",
       },
     },
   },
